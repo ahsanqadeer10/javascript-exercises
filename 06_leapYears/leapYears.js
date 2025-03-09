50b26d08f664b83ededa92742c86af9b69146d5e
Ahsan Qadeer <21953963+ahsanqadeer10@users.noreply.github.com>
2025-03-09 16:29:43 +0500

    Solve 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisibleByFour = year % 4 === 0;
+    divisibleByHundred = year % 100 === 0;
+    divisibleByFourHundred = year % 400 === 0;
 
+    return divisibleByFour && (!divisibleByHundred || divisibleByFourHundred);
 };
 
 // Do not edit below this line
