4e8200985f2e6ab7a702fcbdb871c826cb2c1a4e
Ahsan Qadeer <21953963+ahsanqadeer10@users.noreply.github.com>
2025-03-09 15:27:25 +0500

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    numbersToRemove = Array.prototype.slice.call(arguments, 1);
+    filteredArray = array.filter(x => !numbersToRemove.includes(x));
+    return filteredArray;
 };
 
 // Do not edit below this line
